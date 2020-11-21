0c31d10fc42b81bef35fb30eec62f4470c5b5f03
Mochammad Faishal <72900665+fshll666@users.noreply.github.com>
2020-11-21 20:15:45 +0700

    Add files via upload

---
+++ PBOIF2-10119048-Latihan53-Rabbit/src/pboif2/pkg10119048/latihan53/rabbit/PBOIF210119048Latihan53Rabbit.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan53.rabbit;
+
+/**
+ *
+ * @author SystemOs
+ */
+public class PBOIF210119048Latihan53Rabbit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Rabbit rabbit = new Rabbit("Peter",false,"grass",4,"grey");
+        
+       System.out.println("Hello, his name is " + rabbit.getName());
+       System.out.println(rabbit.getName() + " is Vegetarian? " + rabbit.isVegetarian());
+       System.out.println(rabbit.getName() + " eats " + rabbit.getEats());
+       System.out.println(rabbit.getName() + " has " + rabbit.getNoOfLegs() + " legs.");
+       System.out.println(rabbit.getName() + " color is " + rabbit.getColor());
+    }
+    
+}
